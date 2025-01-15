d4d9cf8701ec4556d77bdbd6d6ab268c6f868e46
DevMboo <lchavesdesousa@gmail.com>
2025-01-14 23:21:58 -0300

    feat: ajustado regras da url

--- index.php
+++ index.php
@@ -4,5 +4,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\config\Bootstrap;
 
+$current_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+if ($current_url == 'http://127.0.0.1:8000/') {
+    header('Location: /listar');
+    exit();
+}
+
 $bootstrap = new Bootstrap();
 $bootstrap->handle();
